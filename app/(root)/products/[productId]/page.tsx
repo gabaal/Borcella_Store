e45cd257a211fa3b9bd6d2ef30acc3bcd1f0594e
gerald <geraldbaalham@live.co.uk>
2024-07-20 19:27:19 +0100

    starting on heart component 5:42:16

---
+++ app/(root)/products/[productId]/page.tsx
@@ -0,0 +1,20 @@
+import Gallery from "@/components/Gallery";
+import ProductInfo from "@/components/ProductInfo";
+import { getProductDetails } from "@/lib/actions";
+
+const productDetails = async ({
+  params,
+}: {
+  params: { productId: string };
+}) => {
+  const productDetails = await getProductDetails(params.productId);
+
+  return (
+    <div className="flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
+      <Gallery productMedia={productDetails.media} />
+      <ProductInfo productInfo={productDetails} />
+    </div>
+  );
+};
+
+export default productDetails;
