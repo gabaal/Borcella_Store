bd4556ed854abf3e0d8528018fd88af398384ecb
gerald <geraldbaalham@live.co.uk>
2024-07-25 11:36:13 +0100

    key for orders

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,3 +1,4 @@
+import ProductList from "@/components/ProductList";
 import { getOrders } from "@/lib/actions/actions";
 
 import { auth } from "@clerk/nextjs/server";
@@ -19,7 +20,10 @@ const Orders = async () => {
 
       <div className="flex flex-col gap-10">
         {orders?.map((order: OrderType) => (
-          <div className="flex flex-col gap-8 p-4 hover:bg-grey-1">
+          <div
+            key={order._id}
+            className="flex flex-col gap-8 p-4 hover:bg-grey-1"
+          >
             <div className="flex gap-20 max-md:flex-col max-md:gap-3">
               <p className="text-base-bold">Order ID: {order._id}</p>
               <p className="text-base-bold">
@@ -29,7 +33,7 @@ const Orders = async () => {
 
             <div className="flex flex-col gap-5">
               {order.products.map((orderItem: OrderItemType) => (
-                <div className="flex gap-4">
+                <div key={orderItem._id} className="flex gap-4">
                   <Image
                     src={orderItem.product.media[0]}
                     alt={orderItem.product.title}
